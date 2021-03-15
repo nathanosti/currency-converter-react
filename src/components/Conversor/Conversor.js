bd797655f6073a40367575e814978ca2790a1aa5
Nathan Osti <nathanosti.job@gmail.com>
2021-03-15 03:04:07 -0300

    Created the base of the converter

--- src/components/Conversor/Conversor.js
+++ src/components/Conversor/Conversor.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
-
 import React, { useState, useEffect } from 'react';
-import './styles.css';
 
+import api from '../../services/api';
+const APIKEY = '[CASO A API TENHA UMA KEY]';
+
+import './styles.css';
 
 export default ({ moedaA, moedaB }) => {
   const [curA, setCurA] = useState('');
@@ -12,19 +14,27 @@ export default ({ moedaA, moedaB }) => {
 
   useEffect(() => {
     setCurA(moedaA);
-    setCurB(moedaB)
-  }, [])
+    setCurB(moedaB);
+    }, []);
 
- function converter() {
-    console.log('linkou')
-  }
+ async function converter() {
+   const de_para = `${moedaA}_${moedaB}`;
+   await api.get(`api/v7/convert?q=${de_para}&compact=ultra&apiKey=${APIKEY}`).then(res => {
+     return res
+   })
+   .then((res) => {
+    const cot = res.data[de_para];
+    const moedaBValue = (parseFloat(curAValue) * cot).toFixed(2);
+    setcurBValue(moedaBValue);
+   })
+  };
 
   return (
     <div className='container'>
       <h1>{curA} para {curB}</h1>
       <input type="number" onChange={(e) => setcurAValue(e.target.value)}/>
       <button type='button' onClick={() => converter()}>Converter</button>
-      <h2>valor convertido</h2>
+      <h2>{curBValue}</h2>
     </div>
   )
 }
